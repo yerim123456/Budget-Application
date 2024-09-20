ef576a9696169dbf8f1a3be3a3fb365facc4c2ba
yerim <97941141+yerim123456@users.noreply.github.com>
2024-09-20 15:28:30 +0900

    feat: member 엔티티 추가 (#3)

---
+++ src/main/java/com/management/budget/module/member/entity/Member.java
@@ -0,0 +1,52 @@
+package com.management.budget.module.member.entity;
+
+import com.management.budget.common.entity.BaseEntity;
+import com.management.budget.module.member.vo.Password;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+public class Member extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String account;
+
+	@Embedded
+	@Column(nullable = false)
+	@Getter(AccessLevel.NONE)
+	private Password password;
+
+	public static Member signUp(String account, String password, PasswordEncoder passwordEncoder) {
+		return Member.builder()
+			.account(account)
+			.password(Password.of(password, passwordEncoder))
+			.build();
+	}
+
+	public String getPassword() {
+		return password.getValue();
+	}
+
+}
+
