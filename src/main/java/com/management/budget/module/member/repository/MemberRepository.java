05d15b3d147d19eb61b8a7f08d4b9590e6d73644
yerim <97941141+yerim123456@users.noreply.github.com>
2024-09-20 15:43:56 +0900

    fix: 잘못된 member import 수정 (#3)

--- src/main/java/com/management/budget/module/member/repository/MemberRepository.java
+++ src/main/java/com/management/budget/module/member/repository/MemberRepository.java
@@ -1,8 +1,8 @@
 package com.management.budget.module.member.repository;
 
-import java.lang.reflect.Member;
 import java.util.Optional;
 
+import com.management.budget.module.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
